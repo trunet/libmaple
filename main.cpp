3b39e3b4c6d83bb836f4195bf8aed0b4fcc9c6c8
Wagner Sartori Junior <wsartori@gmail.com>
2012-04-02 22:57:58 -0300

    BPM085 Working

--- main.cpp
+++ main.cpp
@@ -3,27 +3,37 @@
 
 #include "wirish.h"
 
-#define PWM_PIN  2
+#include <APM_BMP085.h> // APM BMP085 Library
+
+APM_BMP085_Class APM_BMP085;
 
 void setup() {
-    /* Set up the LED to blink  */
     pinMode(BOARD_LED_PIN, OUTPUT);
 
-    /* Turn on PWM on pin PWM_PIN */
-    pinMode(PWM_PIN, PWM);
-    pwmWrite(PWM_PIN, 0x8000);
-
-    /* Send a message out USART2  */
     Serial2.begin(9600);
-    Serial2.println("Hello world!");
+    Serial2.println("TrunetCopter v0.1");
 
-    /* Send a message out the usb virtual serial port  */
-    SerialUSB.println("Hello!");
+	APM_BMP085.Init();   // APM ADC initialization
 }
 
 void loop() {
-    toggleLED();
-    delay(100);
+	float tmp_float;
+	float Altitude;
+	
+	APM_BMP085.Read();
+	Serial2.print("Pressure:");
+	Serial2.print(APM_BMP085.Press);
+    Serial2.print(" Temperature:");
+    Serial2.print(APM_BMP085.Temp/10.0);
+    Serial2.print(" Altitude:");
+    tmp_float = (APM_BMP085.Press/101325.0);
+    tmp_float = pow(tmp_float,0.190295);
+    Altitude = 44330*(1.0-tmp_float);
+	Serial2.print(Altitude);
+	Serial2.println();
+	
+	toggleLED();
+	delay(1000);
 }
 
 // Force init to be called *first*, i.e. before static object allocation.
